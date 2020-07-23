dc1389fdd39c0ff26a921ea33d9f112a7b2706d5
Ben Wang <wyqmlm66@gmail.com>
2020-07-23 23:04:15 +1000

    Q2

---
+++ Codes/src/Q2.java
@@ -0,0 +1,41 @@
+public class Q2 {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        ListNode current = head;
+        int sum = 0;
+        while (l1 != null || l2 != null) {
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            } 
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            current.next = new ListNode(sum % 10);
+            current = current.next;
+            sum /= 10;
+        }
+        if (sum != 0)
+            current.next = new ListNode(sum);
+        return head.next;
+    }
+
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
